4b787958be542a48c0b159922e5a071450f18b03
jamesagib <jagib07@gmail.com>
2023-03-09 19:11:13 -0800

    made app responsive

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App(props) {
   const [chatMessages, setChatMessages] = useState([])
   const [isOpen, setIsOpen] = useState(false)
   const [specialty, setSpecialty] = useState("⛑️ Medical Assistant")
+  const [width, setWindowWidth] = useState(0);
+  const [isMobile, setIsMobile] = useState(false)
 
   const configuration = new Configuration({
     apiKey: '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'
@@ -21,6 +23,10 @@ function App(props) {
   const openai = new OpenAIApi(configuration)
 
   const specialties = [
+    {
+      "name": "📋 Symptom Checker",
+      "role": "Asked symptoms and you will respond with a diagnosis"
+    },
     {
       "name": "⛑️ Medical Assistant",
       "role": "General medical care and first aid"
@@ -101,38 +107,63 @@ function App(props) {
   const mouseLeave = (e) => {
     e.target.style.backgroundColor = 'white'
   }
-  
-  
+
+
+  useEffect(() => { 
+
+    updateDimensions();
+
+    window.addEventListener('resize', updateDimensions);
+    return () => 
+      window.removeEventListener('resize',updateDimensions);
+      
+   }, [])
+
+  const updateDimensions = () => {
+    const width = window.innerWidth
+    setWindowWidth(width)
+    if(width > 1023) {
+      setIsMobile(false)
+    }
+    else {
+      setIsMobile(true)
+    }
+  }
+
+  console.log(width, isMobile)
+
   return (
     <div className="App">
       <header className="App-header">
-        {specialty ? <div style={{ display: 'flex', width: '30rem'}}>
-          <IoMdSettings size={23} color="black" style={{ width: '2rem', height: '2rem', backgroundColor: '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} />
-          <p style={{ fontWeight: '600', width: '30rem', textAlign: 'left', padding: 10, borderRadius: 10, color: 'black', fontSize: 12, backgroundColor: '#EEEE', marginBottom: 20}}>System Instruction: {specialty}</p>
-        </div>
-        : null
-        }
-        {chatMessages.map((item, index) => (
-          <>
-          <div style={{ width: '30rem', display: 'flex', flexDirection: 'row',  justifyContent: 'flex-start', marginBottom: 20}}>
-            {index % 2 === 0 ? <BsFillPersonFill size={23} color="black" style={{ width: '2rem', height: '2rem', backgroundColor: '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} /> : <BiHealth size={23} color="black" style={{ width: '2rem', height: '2rem', backgroundColor: '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} />}
-            <p key={index} style={{ fontWeight: '600', width: '100%', textAlign: 'left', padding: 10, borderRadius: 10, color: index % 2 === 0 ? 'white' : 'black', fontSize: 12, backgroundColor: index % 2 === 0 ? '#147efb' : '#EEEE',}}>{item}</p>
+        <div className="bottom" style={{ width: isMobile ? '93%' : '40%' }}>
+          {specialty ? <div style={{ marginTop: 'auto', display: 'flex', width: '100%'}}>
+            <IoMdSettings size={23} color="black" style={{ width: 35, height: 35, backgroundColor: '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} />
+            <p style={{ fontWeight: '600', width: '100%', textAlign: 'left', padding: 10, borderRadius: 10, color: 'black', fontSize: 12, backgroundColor: '#EEEE', marginBottom: 20}}>System Instruction: {specialty}</p>
           </div>
-          </>
-        ))}
-        <div style={{ position: 'relative', width: '30rem', height: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
-          <input style={{ width: '100%', height: '100%', backgroundColor: 'transparent', borderRadius: 4, borderWidth: 2, borderColor: '#EEEE', padding: 6, color: 'black', fontSize: 12, boxSizing: 'border-box' }} onKeyDown={(event) => {event.code === 'Enter' && prompt != '' ? generateResponse() : console.log() }} placeholder="Ask a medical question..." value={prompt} onChange={(event) => setPrompt(event.target.value)} />
-          <button onClick={() => setIsOpen((prev) => !prev)} style={{ fontWeight: '600', fontSize: 12, height: '1.96rem', width: specialty ? '10rem' : '6rem', position: 'absolute', backgroundColor: '#1f7efb', right: 0.5, borderTopRightRadius: 4, borderBottomRightRadius: 4, alignItems: 'center', justifyContent: 'center', display: 'flex' }}>{specialty ? specialty : 'Normal' } {isOpen ? <AiFillCaretUp size={13} style={{ marginLeft: 4}} /> : <AiFillCaretDown size={13} style={{ marginLeft: 4}} />} </button>
-          {isOpen ? 
-            <div ref={ref} style={{ backgroundColor: 'white', display: 'block', width: '8rem', height: '25rem', borderWidth: 1, borderColor: '#EEEE', borderRadius: 4, marginTop: '28rem', position: 'absolute', right: 0.5}}>
-                {specialties.map((item, index) => (
-                  <button key={index} style={{ color: 'black', fontSize: 12, marginBottom: 2, padding: 5, width: '100%', height: '3rem'}} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={(value) => showSpecialty(value)}>{item.name}</button>
-                ))}
-            </div> 
-            : null
+          : null
           }
+          {chatMessages.map((item, index) => (
+            <>
+            <div style={{ marginTop: 'auto', width: '100%', display: 'flex', flexDirection: 'row',  justifyContent: 'flex-start', marginBottom: 20}}>
+              {index % 2 === 0 ? <BsFillPersonFill size={23} color="black" style={{ width: 35, height: 35, backgroundColor: '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} /> : <BiHealth size={23} color="black" style={{ width: '2rem', height: '2rem', backgroundColor: '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} />}
+              <p key={index} style={{ fontWeight: '600', width: '100%', textAlign: 'left', padding: 10, borderRadius: 10, color: index % 2 === 0 ? 'white' : 'black', fontSize: 12, backgroundColor: index % 2 === 0 ? '#147efb' : '#EEEE',}}>{item}</p>
+            </div>
+            </>
+          ))}
+          <div style={{ marginTop: 'auto', position: 'relative', width: '100%', height: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+            <input style={{ paddingLeft: isMobile ? 260 : 250, width: '100%', height: '100%', backgroundColor: 'transparent', borderRadius: 4, borderWidth: 2, borderColor: '#EEEE', padding: 6, color: 'black', fontSize: 12, boxSizing: 'border-box' }} onKeyDown={(event) => {event.code === 'Enter' && prompt != '' ? generateResponse() : console.log() }} placeholder="Ask a medical question..." value={prompt} onChange={(event) => setPrompt(event.target.value)} />
+            <button onClick={() => setIsOpen((prev) => !prev)} style={{ color: 'white', fontWeight: '600', fontSize: 12, height: '1.96rem', width: isMobile ? '40%' : '31%', position: 'absolute', backgroundColor: '#1f7efb', left: 0.5, borderTopLeftRadius: 4, borderBottomLeftRadius: 4, alignItems: 'center', justifyContent: 'center', display: 'flex' }}>{specialty ? specialty : 'Normal' } {isOpen ? <AiFillCaretUp size={13} style={{ marginLeft: 4}} /> : <AiFillCaretDown size={13} style={{ marginLeft: 4}} />} </button>
+            {isOpen ? 
+              <div ref={ref} style={{ backgroundColor: 'white', display: 'block', width: '31%', height: '1310%', borderWidth: 1, borderColor: '#EEEE', borderRadius: 4, marginTop: '-93%', position: 'absolute', left: 0.5}}>
+                  {specialties.map((item, index) => (
+                    <button key={index} style={{ color: 'black', fontSize: 12, marginBottom: 2, padding: 5, width: '100%', height: '10%'}} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={(value) => showSpecialty(value)}>{item.name}</button>
+                  ))}
+              </div> 
+              : null
+            }
+          </div>
+          <p style={{ marginBottom: 20, color: '#BDBDBD', fontSize: 10, marginTop: 3, width: '100%' }}>This tool does not provide medical advice, it is intended for informational purposes only. If you think you may have a medical emergency, immediately call your doctor or dial 911.</p>
         </div>
-        <p style={{ marginBottom: 20, color: '#BDBDBD', fontSize: 10, marginTop: 3, width: '28rem' }}>This tool does not provide medical advice, it is intended for informational purposes only. If you think you may have a medical emergency, immediately call your doctor or dial 911.</p>
       </header>
     </div>
   );
