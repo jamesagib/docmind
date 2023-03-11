97785c4d37c242341ca93b4a5df664ba2390091d
jamesagib <jagib07@gmail.com>
2023-03-10 20:52:13 -0800

    removed login

--- src/App.js
+++ src/App.js
@@ -6,17 +6,24 @@ import { BsFillPersonFill } from 'react-icons/bs'
 import { IoMdSettings } from "react-icons/io";
 import { IoIosCheckmarkCircle } from "react-icons/io";
 import { MdDarkMode, MdOutlineDarkMode } from "react-icons/md";
+import Gradient from 'rgt'
+import jwt_decode from "jwt-decode";
 
 function App(props) {
+
+  const [user, setUser ] = useState([]);
+  const [profile, setProfile ] = useState("");
+  const [loggedIn, setLoggedIn] = useState()
   
   const [prompt, setPrompt] = useState("")
   const [response, setResponse] = useState("")
   const [chatMessages, setChatMessages] = useState([])
+  const [chats, setChats] = useState([""])
   const [isOpen, setIsOpen] = useState(false)
   const [specialty, setSpecialty] = useState("⛑️ Medical Assistant")
   const [width, setWindowWidth] = useState(0);
   const [isMobile, setIsMobile] = useState(false)
-  
+
   const [darkMode, setDarkMode] = useState(JSON.parse(localStorage.getItem('darkMode')))
 
   const configuration = new Configuration({
@@ -87,7 +94,7 @@ function App(props) {
     if(specialty === '⛑️ Medical Assistant') { 
       const res = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{"role": "system", "content": "General medical care and first aid"}, {role: "user", content: `${prompt}`},],
+        messages: [{"role": "system", "content": "General medical care and first aid"}, {"role": "user", "content": `${prompt}`},],
       })
 
       chatMessages.push(res.data.choices[0].message.content)
@@ -97,7 +104,7 @@ function App(props) {
     else {
       const res = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{role: "user", content: `${prompt}`},],
+        messages: [{"role": "user", "content": `${prompt}`},],
       })
 
       chatMessages.push(res.data.choices[0].message.content)
@@ -176,11 +183,11 @@ function App(props) {
   }
 
   const mouseEnter = (e) => {
-    e.target.style.backgroundColor = '#EEEE'
+    e.target.style.backgroundColor = darkMode ? '#272727' : '#EEEE'
   }
 
   const mouseLeave = (e) => {
-    e.target.style.backgroundColor = 'white'
+    e.target.style.backgroundColor = darkMode ? '#1e1e1e' : 'white'
   }
 
   useEffect(() => { 
@@ -216,14 +223,14 @@ function App(props) {
     }
   }
 
+
   return (
     <div className="App">
       <header className="App-header" style={{ backgroundColor: darkMode ? '#1e1e1e' : 'white' }}>
         <div style={{ backgroundColor: darkMode ? '#272727' : 'white', height: '40%', width: '100%', boxShadow: '5px 0 30px rgba(1,41,112,0.08)'}}>
-        {darkMode ? <button style={{ float: 'right', padding: 10}}><MdDarkMode style={{ display: 'flex'}} color="white" onClick={changeTheme} /></button> : <button style={{ float: 'right', padding: 10}}><MdOutlineDarkMode color="black" onClick={changeTheme} /></button> }
+        {darkMode ? <button style={{ float: 'right', padding: 10}}><MdDarkMode style={{ display: 'flex'}} color="white" size={30} onClick={changeTheme} /></button> : <button style={{ float: 'right', padding: 10 }}><MdOutlineDarkMode size={30} color="black" onClick={changeTheme} /></button> }
         </div>
-
-        <div style={{ width: '20%',display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 100}}>
+        <div style={{ width: '20%', display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', marginTop: 100}}>
           <img src='./logo.jpeg' style={{ width: 42, height: 42, borderRadius: 7, marginRight: 10}}></img>
           <h1 style={{ color: darkMode ? 'white' : 'black', fontSize: 40, fontWeight: '600' }}>DocMind</h1>
         </div>
@@ -249,7 +256,7 @@ function App(props) {
         </div>
         <div className="bottom" style={{ width: isMobile ? '93%' : '40%' }}>
           {specialty ? <div style={{ marginTop: 'auto', display: 'flex', width: '100%'}}>
-            <IoMdSettings size={23} color="black" style={{ width: 35, height: 35, backgroundColor: darkMode ? 'white' : '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} />
+            <IoMdSettings size={20} color="black" style={{ width: 38, height: 38, backgroundColor: darkMode ? 'white' : '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} />
             <p style={{ fontWeight: '600', width: '100%', textAlign: 'left', padding: 10, borderRadius: 10, color: 'black', fontSize: 12, backgroundColor: darkMode ? 'white' : '#EEEE', marginBottom: 20}}>System Instruction: {specialty}</p>
           </div>
           : null
@@ -257,18 +264,18 @@ function App(props) {
           {chatMessages.map((item, index) => (
             <>
             <div style={{ marginTop: 'auto', width: '100%', display: 'flex', flexDirection: 'row',  justifyContent: 'flex-start', marginBottom: 20}}>
-              {index % 2 === 0 ? <BsFillPersonFill size={23} color="black" style={{ width: 35, height: 35, backgroundColor: darkMode ? 'white' : '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} /> : <img src='./logo.jpeg' style={{ width: 35, height: 35, marginRight: 8, padding: 3, borderRadius: 7}}></img>}
+              {index % 2 === 0 ? <BsFillPersonFill size={23} color="black" style={{ width: 38, height: 38, backgroundColor: darkMode ? 'white' : '#EEEE', marginRight: 8, padding: 3, borderRadius: 4 }} /> : <img src='./logo.jpeg' style={{ width: 38, height: 38, marginRight: 8, padding: 3, borderRadius: 7}}></img>}
               <p key={index} style={{ fontWeight: '600', width: '100%', textAlign: 'left', padding: 10, borderRadius: 10, color: index % 2 === 0 ? 'white' : 'black', fontSize: 12, backgroundColor: index % 2 === 0 ? '#147efb' : 'white',}}>{item}</p>
             </div>
             </>
           ))}
-          <div style={{ marginTop: 'auto', position: 'relative', width: '100%', height: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
+          <div style={{ marginTop: 'auto', position: 'relative', width: '100%', height: 35, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center'}}>
             <input style={{ paddingLeft: 10, width: '100%', height: '100%', backgroundColor: 'transparent', borderRadius: 4, borderWidth: 2, borderColor: '#EEEE', padding: 6, color: darkMode ? 'white' : '#EEEE', fontSize: 12, boxSizing: 'border-box' }} onKeyDown={(event) => {event.code === 'Enter' && prompt != '' ? generateResponse() : console.log() }} placeholder="Ask a medical question..." value={prompt} onChange={(event) => setPrompt(event.target.value)} />
             <button onClick={() => setIsOpen((prev) => !prev)} style={{ color: 'white', fontWeight: '600', fontSize: 12, height: '1.96rem', width: isMobile ? '40%' : '31%', position: 'absolute', backgroundColor: '#1f7efb', right: 0.5, borderTopRightRadius: 4, borderBottomRightRadius: 4, alignItems: 'center', justifyContent: 'center', display: 'flex' }}>{specialty ? specialty : 'Normal' } {isOpen ? <AiFillCaretUp size={13} style={{ marginLeft: 4}} /> : <AiFillCaretDown size={13} style={{ marginLeft: 4}} />} </button>
             {isOpen ? 
-              <div ref={ref} style={{ backgroundColor: 'white', display: 'block', width: '31%', height: isMobile ? '1270%' : '1600%', borderWidth: 1, borderColor: '#EEEE', borderRadius: 4, marginTop: isMobile ? -450 : -550, position: 'absolute', right: 0.5}}>
+              <div ref={ref} style={{ backgroundColor: darkMode ? "#1e1e1e" : 'white', display: 'block', width: '31%', height: isMobile ? '1270%' : '1600%', borderWidth: 1, borderColor: '#EEEE', borderRadius: 4, marginTop: isMobile ? -450 : -550, position: 'absolute', right: 0.5}}>
                   {specialties.map((item, index) => (
-                    <button key={index} style={{ color: 'black', fontSize: 12, marginBottom: 2, padding: 5, width: '100%', height: '10%'}} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={(value) => showSpecialty(value)}>{item.name}</button>
+                    <button key={index} style={{ color: darkMode ? 'white' : 'black', fontSize: 12, marginBottom: 2, padding: 5, width: '100%', height: '10%'}} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave} onClick={(value) => showSpecialty(value)}>{item.name}</button>
                   ))}
               </div> 
               : null
@@ -277,9 +284,10 @@ function App(props) {
           <p style={{ marginBottom: 60, color: darkMode ? 'white' : 'grey', fontSize: 10, marginTop: 3, width: '100%' }}>This tool does not provide medical advice, it is intended for informational purposes only. If you think you may have a medical emergency, immediately call your doctor or dial 911.</p>
         </div>
       </header>
-    </div>
+    </div> 
   );
 }
+
 
 export default App;
 
